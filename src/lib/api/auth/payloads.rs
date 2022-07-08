06f51f27860577524913487b8fe5db051dabec99
nayyara-airlangga <nayyara-airlangga@gmail.com>
2022-07-09 00:21:10 +0700

    feat(payload): create register payload

--- src/lib/api/auth/payloads.rs
+++ src/lib/api/auth/payloads.rs
@@ -6,3 +6,12 @@ pub struct LoginPayload {
     pub password: String,
     pub remember_me: Option<bool>,
 }
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct RegisterPayload {
+    pub username: String,
+    pub email: String,
+    pub name: String,
+    pub password: String,
+    pub remember_me: Option<bool>,
+}
