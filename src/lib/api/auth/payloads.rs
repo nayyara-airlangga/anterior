1c26356f26be4e6218ce38dd059e57dc4dcd4098
nayyara-airlangga <nayyara-airlangga@gmail.com>
2022-07-08 23:49:18 +0700

    feat: create login payload struct

--- src/lib/api/auth/payloads.rs
+++ src/lib/api/auth/payloads.rs
@@ -1,10 +1,8 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct RegisterPayload {
+pub struct LoginPayload {
     pub username: String,
-    pub email: String,
-    pub name: String,
     pub password: String,
     pub remember_me: Option<bool>,
 }
