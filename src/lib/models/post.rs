4c680457fd82a31e267e75543b72b507305e7a33
nayyara-airlangga <nayyara-airlangga@gmail.com>
2022-07-10 17:37:38 +0700

    feat: create post models

---
+++ src/lib/models/post.rs
@@ -0,0 +1,35 @@
+use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
+
+use super::user::User;
+
+#[derive(FromRow, Debug, Deserialize, Serialize)]
+pub struct Post {
+    pub title: String,
+    pub headline: String,
+    pub slug: String,
+    pub content: String,
+    pub published: bool,
+
+    pub created_at: chrono::DateTime<chrono::Local>,
+    pub edited_at: chrono::DateTime<chrono::Local>,
+    pub published_at: Option<chrono::DateTime<chrono::Local>>,
+
+    pub author_id: i32,
+}
+
+#[derive(FromRow, Debug, Deserialize, Serialize)]
+pub struct PostWithAuthor {
+    pub title: String,
+    pub headline: String,
+    pub slug: String,
+    pub content: String,
+    pub published: bool,
+
+    pub created_at: chrono::DateTime<chrono::Local>,
+    pub edited_at: chrono::DateTime<chrono::Local>,
+    pub published_at: Option<chrono::DateTime<chrono::Local>>,
+
+    pub author_id: i32,
+    pub author: User,
+}
