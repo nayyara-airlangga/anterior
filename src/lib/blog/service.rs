7d569994589faaed45c8be9aeae96001f32e17d3
nayyara-airlangga <nayyara-airlangga@gmail.com>
2022-07-21 14:23:02 +0700

    refactor: use post_count to minimize call to len

--- src/lib/blog/service.rs
+++ src/lib/blog/service.rs
@@ -31,23 +31,22 @@ impl BlogService {
             }
         };
 
+        let post_count = posts.len();
         let mut has_next = false;
 
-        if posts.len() > query.limit as usize {
+        if post_count > query.limit as usize {
             has_next = true;
             posts = posts.into_iter().take(query.limit as usize).collect();
         }
 
-        let post_count = posts.len();
-
-        let cursor = if posts.len() > 0 {
+        let cursor = if post_count > 0 {
             Some(posts.get(post_count - 1).unwrap().id)
         } else {
             None
         };
 
         let metadata = Metadata {
-            count: posts.len() as i32,
+            count: post_count as i32,
             pagination: Pagination { has_next, cursor },
         };
 
