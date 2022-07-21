ab44706d1a981d6dbd827b0c58302fbb00e3d17b
nayyara-airlangga <nayyara-airlangga@gmail.com>
2022-07-21 15:00:44 +0700

    feat: create post detail model

--- src/lib/blog/repository.rs
+++ src/lib/blog/repository.rs
@@ -21,10 +21,8 @@ impl BlogRepository {
 
         let query_str = format!(
             r#"
-SELECT posts.id "p_id", title, headline, slug, published, posts.created_at "p_created_at", edited_at, published_at, author_id, users.id "u_id", username, name, email, users.created_at "u_created_at"
+SELECT posts.id, title, headline, slug, published, posts.created_at, edited_at, published_at
 FROM posterior.posts AS posts
-LEFT JOIN posterior.users AS users
-ON posts.author_id = users.id
 WHERE {}
 ORDER BY posts.created_at DESC, edited_at DESC, title ASC
 LIMIT $1
--- src/lib/models/post.rs
+++ src/lib/models/post.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use sqlx::{postgres::PgRow, Row};
 
-use super::Metadata;
+use super::{Metadata, User};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Post {
@@ -19,14 +19,55 @@ pub struct Post {
 impl From<PgRow> for Post {
     fn from(row: PgRow) -> Self {
         Post {
+            id: row.get("id"),
+            title: row.get("title"),
+            headline: row.get("headline"),
+            slug: row.get("headline"),
+            published: row.get("published"),
+            created_at: row.get("created_at"),
+            edited_at: row.get("edited_at"),
+            published_at: row.get("published_at"),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct PostDetail {
+    pub id: i32,
+    pub title: String,
+    pub headline: String,
+    pub slug: String,
+    pub published: bool,
+    pub content: String,
+
+    pub created_at: chrono::DateTime<chrono::Local>,
+    pub edited_at: chrono::DateTime<chrono::Local>,
+    pub published_at: Option<chrono::DateTime<chrono::Local>>,
+
+    pub author_id: i32,
+    pub author: User,
+}
+
+impl From<PgRow> for PostDetail {
+    fn from(row: PgRow) -> Self {
+        PostDetail {
             id: row.get("p_id"),
             title: row.get("title"),
             headline: row.get("headline"),
             slug: row.get("headline"),
             published: row.get("published"),
+            content: row.get("content"),
             created_at: row.get("p_created_at"),
             edited_at: row.get("edited_at"),
             published_at: row.get("published_at"),
+            author_id: row.get("author_id"),
+            author: User {
+                id: row.get("u_id"),
+                username: row.get("username"),
+                name: row.get("name"),
+                email: row.get("email"),
+                created_at: row.get("u_created_at"),
+            },
         }
     }
 }
